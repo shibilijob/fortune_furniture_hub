fa752d9787f3685d032988caaea9bcbcd70c6010
Muhammed Shibili <shibilijob@gmail.com>
2026-03-07 11:42:42 +0530

    base url changed

--- frontend/src/api/API.js
+++ frontend/src/api/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/user",
+  baseURL:"https://fortune-furniture-hub-tg79.vercel.app/",
   withCredentials: true
 })
 
