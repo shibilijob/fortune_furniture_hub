cec6f091ac551dc8c5ed2a14d47687ac51d1ff3c
Muhammed Shibili <shibilijob@gmail.com>
2026-03-07 17:43:35 +0530

    mongo db replace

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,12 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
-const connectDB = async () => {
-
-  if (isConnected) {
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = db.connections[0].readyState;
-
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log("MongoDB connection error:", error.message);
-  }
-};
-
+const connectDB =async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('mongodb connected');
+        
+    } catch (error) {
+        console.log('mongodb not connected',error.message)
+    }
+}
 export default connectDB;
